9fee5162413f078fb17a767f9d8d23bd0ee0c88b
김서현 <user@MacBookAir.Dlink>
2025-09-20 20:30:29 +0900

    업로드 후 다음 화면으로 이동

--- main/views.py
+++ main/views.py
@@ -1,11 +1,19 @@
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
+import os
 
 def home(request):
     if request.method == "POST" and request.FILES.get("file"):
         uploaded_file = request.FILES["file"]
-        with open(f"media/{uploaded_file.name}", "wb+") as destination:
+        save_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
+
+        with open(save_path, "wb+") as destination:
             for chunk in uploaded_file.chunks():
                 destination.write(chunk)
-        return HttpResponse("업로드 성공 🎉")
+
+        return redirect("success")
     return render(request, "home.html")
+
+def success(request):
+    return render(request, "success.html")
