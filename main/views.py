e1f1576f5f63566159c2c8419fb605ecfcacc18a
김서현 <user@MacBookAir.Dlink>
2025-09-21 10:22:39 +0900

    처리방침 텍스트박스 추가

--- main/views.py
+++ main/views.py
@@ -1,19 +1,27 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
 import os
 
 def home(request):
-    if request.method == "POST" and request.FILES.get("file"):
-        uploaded_file = request.FILES["file"]
-        save_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
+    uploaded_file = None
 
+    # 파일 업로드만 처리
+    if request.method == "POST" and "apk_file" in request.FILES:
+        apk_file = request.FILES["apk_file"]
+
+        # 파일 저장
+        save_path = os.path.join(settings.MEDIA_ROOT, apk_file.name)
         with open(save_path, "wb+") as destination:
-            for chunk in uploaded_file.chunks():
+            for chunk in apk_file.chunks():
                 destination.write(chunk)
 
-        return redirect("success")
-    return render(request, "home.html")
+        uploaded_file = apk_file.name
+
+    return render(request, "home.html", {"uploaded_file": uploaded_file})
+
 
 def success(request):
+    if request.method == "POST":
+        policy_text = request.POST.get("policy_text")
+        return render(request, "success.html", {"policy_text": policy_text})
     return render(request, "success.html")
