b47cca00f46ff05737b4972e84cfe9ee08ef6a4c
김서현 <user@MacBookAir.Dlink>
2025-10-30 22:08:34 +0900

    commit

--- analyzer/test_policy_predict.py
+++ analyzer/test_policy_predict.py
@@ -1,6 +1,6 @@
 from policy_predict import predict_texts
 if __name__ == "__main__":
-    test_text = "카메라와 위치 정보를 수집해서 사용자 맞춤 서비스를 제공합니다."
+    test_text = "카메라와 위치 정보를 수집해서 사용자 맞춤 서비스를 제공합니다. 계정정보는 수집하지 않습니다."
     try:
         result = predict_texts(test_text)
         print("정상 예측 결과:", result)
